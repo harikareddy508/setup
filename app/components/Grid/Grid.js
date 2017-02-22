ad66d9a7721e5b6a9dcc641f6ec63f6c2c85e771
harikareddy508 <harikareddy.bollapalli@gmail.com>
2017-02-21 22:20:01 -0600

    Adding Grid get and modal

--- app/components/Grid/Grid.js
+++ app/components/Grid/Grid.js
@@ -1,6 +1,31 @@
 import React, { PropTypes, Component } from 'react'
 import DataGrid from 'react-data-grid'
+import Modal from 'simple-react-modal'
 import { Toolbar } from 'react-data-grid-addons'
+
+class ColumnModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+      data: [],
+      selectedRow: {},
+    }
+  }
+  handleShowModal = (e) => {
+    this.setState({
+      show: true,
+    })
+  }
+
+  render() {
+    return (
+      <div className="stats">
+        <i className="fa fa-history" onClick={this.handleShowModal} />
+      </div>
+    )
+  }
+}
 
 export default class Grid extends Component {
   constructor(props) {
@@ -8,36 +33,43 @@ export default class Grid extends Component {
     this.state = {
       columns: [
         {
-          key: 'id',
-          name: 'ID',
-          width: 80,
+          key: 'Manager Class',
+          name: 'Manager Class',
           filterable: true
         },
         {
-          key: 'age',
-          name: 'Actual Age',
-          filterable: false
+          key: 'Alert Name',
+          name: 'Alert Name',
+          filterable: true
         },
         {
-          key: 'name',
-          name: 'Name',
+          key: 'Alert Description',
+          name: 'Alert Description',
+          filterable: true
+        },
+        {
+          key: "stats",
+          name: "Stats",
+          formatter: ColumnModal,
+        },
+        {
+          key: 'Considerations',
+          name: 'Considerations',
           filterable: true
         }
       ]
     }
   }
 
-  rowGetter(rowIndex) {
-    // this.props.data[rowIndex]
-    return {
-      id: rowIndex,
-      name: 'Name '+rowIndex,
-      age: 'Real Age: '+rowIndex,
-    }
+
+  rowGetter = (rowIndex) => {
+    const { data } = this.props
+    const rowData = data[rowIndex];
+    return rowData
   }
 
   getSize() {
-    return 15;
+    return this.props.data.length;
   }
 
   handleFilterChange(filter) {
@@ -48,17 +80,99 @@ export default class Grid extends Component {
     console.log('Clear Filter')
   }
 
+  onCellSelected = ({ rowIdx, idx }) => {
+    if (idx === 3) {
+      this.setState({
+        show: true,
+        selectedRowId: rowIdx,
+        selectedColumnId: idx,
+        selectedRow: this.props.data[rowIdx],
+      })
+    }
+  }
+
+  modalClose = () => {
+    this.setState({
+      show: false,
+    })
+  }
+
   render() {
+    const { data } = this.props
     return (
-      <DataGrid
-        columns={this.state.columns}
-        rowGetter={this.rowGetter}
-        enableCellSelect={true}
-        rowsCount={this.getSize()}
-        minHeight={400}
-        toolbar={<Toolbar enableFilter={true}/>}
-        onAddFilter={this.handleFilterChange}
-        onClearFilters={this.onClearFilters} />
+      <div>
+        <Modal show={this.state.show} onClose={this.modalClose} transitionSpeed={500}>
+          <div className="info">
+            <div className="row">
+              <div className="name">
+                2016 Alerts
+              </div>
+              <div className="value">
+                {this.state.selectedRow && this.state.selectedRow["2016 Alerts"]}
+              </div>
+            </div>
+            <div className="row">
+              <div className="name">
+                2016 Alerts Displayed
+              </div>
+              <div className="value">
+                {this.state.selectedRow && this.state.selectedRow["2016 Alerts Displayed"]}
+              </div>
+            </div>
+              <div className="row">
+                <div className="name">
+                  YTD Alerts
+                </div>
+                <div className="value">
+                  {this.state.selectedRow && this.state.selectedRow["YTD Alerts"]}
+                </div>
+            </div>
+              <div className="row">
+                <div className="name">
+                  YTD Alerts Displayed
+                </div>
+                <div className="value">
+                  {this.state.selectedRow && this.state.selectedRow["YTD Alerts Displayed"]}
+                </div>
+             </div>
+              <div className="row">
+                <div className="name">
+                  2016 Year Tickets
+                </div>
+                <div className="value">
+                  {this.state.selectedRow && this.state.selectedRow["2016 Year Tickets"]}
+                </div>
+              </div>
+               <div className="row">
+                <div className="name">
+                  YTD Tickets
+                </div>
+                <div className="value">
+                  {this.state.selectedRow && this.state.selectedRow["YTD Tickets"]}
+                </div>
+            </div>
+             <div className="row">
+                <div className="name">
+                  Mitigated Alerts Based On Time Threshold
+                </div>
+                <div className="value">
+                  {this.state.selectedRow && this.state.selectedRow["Mitigated Alerts Based On Time Threshold"]}
+                </div>
+            </div>
+          </div>
+        </Modal>
+        <DataGrid
+          columns={this.state.columns}
+          rowGetter={this.rowGetter}
+          enableCellSelect={true}
+          rowsCount={this.getSize()}
+          minHeight={385}
+          toolbar={<Toolbar enableFilter={true}/>}
+          onAddFilter={this.handleFilterChange}
+          enableCellSelect
+          onCellSelected={this.onCellSelected}
+          onClearFilters={this.onClearFilters} />
+      </div>
     )
   }
 }
--- app/components/Layout/Layout.js
+++ app/components/Layout/Layout.js
@@ -27,6 +27,7 @@ export default class Layout extends Component {
       alarmOptions: [],
       data: [],
       visible: false,
+      visibleOptions: [],
     }
   }
 
@@ -63,26 +64,38 @@ export default class Layout extends Component {
   }
 
   parseResponse(json) {
-    console.log(json)
-    const options = []
-    for(var i = 0; i< json.length; i++) {
-      options.push({
-        label: json[i].name,
-        value: json[i].id,
-      })
-    }
     this.setState({
-      alarmOptions: options
+      alarmOptions: json
     })
   }
 
+  constructOptions(arrayOfOptions) {
+    const options = []
+    for(var i = 0; i< arrayOfOptions.length; i++) {
+      options.push({
+        label: arrayOfOptions[i].name,
+        value: arrayOfOptions[i].id,
+      })
+    }
+    return options;
+  }
+
   componentWillMount() {
-    fetch('url').then((response) => {
+    fetch('public/Jsonfile/alarms.json').then((response) => {
       return response.json()
     }).then((json) => {
       this.parseResponse(json)
+      // this.setState({
+      //   visibleOptions: this.constructOptions(this.state.alarmOptions.slice(0, 50))
+      // })
     }).catch((error) => {
       console.log(error)
+    })
+    fetch('public/Jsonfile/smartAlarmingGridData.json').then(response => response.json()).then((json) => {
+      console.log(json);
+      this.setState({
+        data: json,
+      })
     })
   }
 
@@ -110,6 +123,16 @@ export default class Layout extends Component {
     })
   }
 
+  getOptions = (input, callback) => {
+    let filteredResults = this.state.alarmOptions.filter((option) => {
+          return option.name.toLowerCase().startsWith(input.toLowerCase())
+    }).slice(0, 50);
+    if (input === "") {
+      filteredResults = this.state.alarmOptions.slice(0, 50);
+    }
+    callback(null, { options: this.constructOptions(filteredResults) });
+  }
+
   render() {
     return (
       <div className="Layout">
@@ -135,17 +158,18 @@ export default class Layout extends Component {
                   <div className="container col-md-9">
                       <div className="alarm-group">
                         <label className="alarm-name">Alarm Name:</label>
-                        <Select
+                        <Select.Async
                           className="select-alarm-name"
                           name="form-field-name"
                           value={this.state.alarm}
-                          options={options}
+                          loadOptions={this.getOptions}
                           onChange={this.handleAlarmChange}
+                          options={this.state.visibleOptions}
                         />
                       </div>
                       <div className="grid">
                         {/*<Grid data={this.state.data}/> */}
-                        <Grid />
+                        <Grid data={this.state.data} />
                       </div>
                     </div>
                     <div className="inner-container col-md-3">
@@ -162,37 +186,47 @@ export default class Layout extends Component {
                             <Tab>OLA</Tab>
                           </TabList>
                           <TabPanel>
-                            <div className="time-group">
-                              <label className="time-stamp">Time Stamp:</label>
+                            <div className="assign-group">
+                              <label className="groupassign">Group Assign:</label>
                               <Select
-                                className="select-time-stamp"
+                                className="select-group-assign"
                                 name="form-field-name"
                                 value={this.state.time}
                                 options={times}
                                 onChange={this.handleTimeChange}
                               />
                             </div>
-                            <div className="radio-group">
-                              <label className="callout">Call Out:</label>
-                              <ButtonGroup>
-                                  <Radio inline name="groupOptions" onChange={() => this.handleCheckbox('option 1')}>Option 1</Radio>
-                                  <Radio inline name="groupOptions" onChange={() => this.handleCheckbox('option 2')}>Option 2</Radio>
-                              </ButtonGroup>
-                            </div>
-                            <button className="submit-button" onClick={this.sendToDB} type="submit">Submit</button>
-                          </TabPanel>
-                          <TabPanel>
-                            OLA
-                          </TabPanel>
-                        </Tabs>
-                      </div>
+                              <div className="time-group">
+                                <label className="time-stamp">Time Stamp:</label>
+                                <Select
+                                  className="select-time-stamp"
+                                  name="form-field-name"
+                                  value={this.state.time}
+                                  options={times}
+                                  onChange={this.handleTimeChange}
+                                />
+                              </div>
+                              <div className="radio-group">
+                                <label className="callout">Call Out:</label>
+                                <ButtonGroup>
+                                    <Radio inline name="groupOptions" onChange={() => this.handleCheckbox('option 1')}>Option 1</Radio>
+                                    <Radio inline name="groupOptions" onChange={() => this.handleCheckbox('option 2')}>Option 2</Radio>
+                                </ButtonGroup>
+                              </div>
+                              <button className="submit-button" onClick={this.sendToDB} type="submit">Submit</button>
+                            </TabPanel>
+                            <TabPanel>
+                              OLA
+                            </TabPanel>
+                          </Tabs>
+                        </div>
+                    </div>
                   </div>
-                </div>
-              </Segment>
-            </Sidebar.Pusher>
-          </Sidebar.Pushable>
-        </div>
-    </div>
-    )
+                </Segment>
+              </Sidebar.Pusher>
+            </Sidebar.Pushable>
+          </div>
+      </div>
+      )
+    }
   }
-}
